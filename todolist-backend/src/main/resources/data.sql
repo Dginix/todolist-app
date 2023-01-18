eca9de06d5422942c6d9aed92283bc42de69e046
dginix <kruchininds@yandex.ru>
2023-01-18 19:12:28 +0500

    fix: Do nothing if values in data.sql already exist

--- todolist-backend/src/main/resources/data.sql
+++ todolist-backend/src/main/resources/data.sql
@@ -1,8 +1,11 @@
-INSERT INTO task (title, description, creation_date, expiration_date, is_done)
-    VALUES ('Task 1', 'BlaBlaBla 111', '2023-01-18 18:23:01.001', '2023-01-19 18:01:01.001', false);
+INSERT INTO task (id, title, description, creation_date, expiration_date, is_done)
+    VALUES (1, 'Task 1', 'BlaBlaBla 111', '2023-01-18 18:23:01.001', '2023-01-19 18:01:01.001', false)
+        ON CONFLICT (id) DO NOTHING;
 
-INSERT INTO task (title, description, creation_date, expiration_date, is_done)
-    VALUES ('Task 2', 'BlaBlaBla 222', '2023-01-18 18:30:01.001', '2023-02-19 18:05:01.001', false);
+INSERT INTO task (id, title, description, creation_date, expiration_date, is_done)
+    VALUES (2, 'Task 2', 'BlaBlaBla 222', '2023-01-18 18:30:01.001', '2023-02-19 18:05:01.001', false)
+            ON CONFLICT (id) DO NOTHING;
 
-INSERT INTO task (title, description, creation_date, expiration_date, is_done)
-    VALUES ('Task 3', 'BlaBlaBla 333', '2023-01-18 18:40:01.001', '2023-03-19 18:10:01.001', false);
+INSERT INTO task (id, title, description, creation_date, expiration_date, is_done)
+    VALUES (3, 'Task 3', 'BlaBlaBla 333', '2023-01-18 18:40:01.001', '2023-03-19 18:10:01.001', false)
+            ON CONFLICT (id) DO NOTHING;
